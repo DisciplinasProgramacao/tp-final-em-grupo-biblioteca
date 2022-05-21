ff1d2d2619e43c07b65720ccd07385dc6f2f45f0
Raissa Maia <83675351+Maiaok@users.noreply.github.com>
2022-05-21 12:18:20 -0300

    class emprestimo

---
+++ emprestimo.java
@@ -0,0 +1,15 @@
+public class class Emprestimo {
+    private livros:Livros;
+    private usuario:Usuarios;
+    private dataEmprestimo: Data; 
+    private dataDevolucao:Data ;
+    private dataPrevistaDevolucao:Data;
+}
+
+public boolean saberAtraso(){
+
+}
+
+public boolean punicao() {
+
+}
