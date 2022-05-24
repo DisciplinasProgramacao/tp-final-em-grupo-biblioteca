fe447deff351310e5cb8c2093d495fec8567f0ca
mateus.oliveira <mateus.oliveira@dtidigital.com.br>
2022-05-23 22:06:13 -0300

    implementação da classe Data

--- biblioteca/src/LivrosFisicosPrioritarios.java
+++ biblioteca/src/LivrosFisicosPrioritarios.java
@@ -1,4 +1,10 @@
-public class LivrosFisicosPrioritarios extends LivrosFisicos{
+public class LivrosFisicosPrioritarios extends LivrosFisicos {
+
     private int qtdeDiasRetencao = 3;
 
+    public LivrosFisicosPrioritarios(String autor, String editora, String titulo, int prazo) {
+        super(autor, editora, titulo, prazo);
+        // TODO Auto-generated constructor stub
     }
+
+}
