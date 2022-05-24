e8127d38345d645cb7b733498d3fc2029844110d
ErikDouglas <douglaserik545@gmail.com>
2022-05-23 21:31:26 -0300

    Classse Usuarios

---
+++ biblioteca/src/Usuarios.java
@@ -0,0 +1,24 @@
+public class Usuarios{
+    private String nome;
+    private String matricula;
+    private String emprestimo;
+
+    public String visualizarHistorico (Data dataInic, Data dataFinal){
+
+    }
+    public String visualizarDadosCadastrais(){
+
+    }
+    public void emprestar( Emprestimo emprestimo){
+
+    }
+    public void devolver (Emprestimo emprestimo){
+
+    }
+    public void renovar (Emprestimo emprestimo){
+
+    }
+    public String listarLivrosAtrasados (){
+
+    }
+}
